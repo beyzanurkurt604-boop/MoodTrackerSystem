4e10b2a0bb6611918ea55db6c811d0a9d9147743
beyzanurkurt604-boop <beyzanurkurt604@gmail.com>
2026-05-21 23:50:27 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace MoodTrackerSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MoodEntry = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbMood = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblTodayMood = new System.Windows.Forms.Label();
+            this.lblSuggestion = new System.Windows.Forms.Label();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.MoodEntry.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoodEntry
+            // 
+            this.MoodEntry.Controls.Add(this.tabPage1);
+            this.MoodEntry.Controls.Add(this.tabPage2);
+            this.MoodEntry.Controls.Add(this.tabPage3);
+            this.MoodEntry.Location = new System.Drawing.Point(26, 12);
+            this.MoodEntry.Name = "MoodEntry";
+            this.MoodEntry.SelectedIndex = 0;
+            this.MoodEntry.Size = new System.Drawing.Size(746, 426);
+            this.MoodEntry.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblSuggestion);
+            this.tabPage1.Controls.Add(this.lblTodayMood);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(738, 397);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dashboard";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnSave);
+            this.tabPage2.Controls.Add(this.txtNote);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.cmbMood);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(738, 397);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mood Entry";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dgvHistory);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(738, 397);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "History";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bugün Nasıl Hissediyorsun?";
+            // 
+            // cmbMood
+            // 
+            this.cmbMood.FormattingEnabled = true;
+            this.cmbMood.Items.AddRange(new object[] {
+            "Harika",
+            "",
+            "",
+            "Mutlu",
+            "",
+            "",
+            "Normal",
+            "",
+            "",
+            "Stresli",
+            "",
+            "",
+            "Üzgün"});
+            this.cmbMood.Location = new System.Drawing.Point(60, 81);
+            this.cmbMood.Name = "cmbMood";
+            this.cmbMood.Size = new System.Drawing.Size(121, 24);
+            this.cmbMood.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(307, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Günlük Notun";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(310, 72);
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(100, 88);
+            this.txtNote.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(501, 38);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(156, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Kaydet ve Öneri Al";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblTodayMood
+            // 
+            this.lblTodayMood.AutoSize = true;
+            this.lblTodayMood.Location = new System.Drawing.Point(72, 43);
+            this.lblTodayMood.Name = "lblTodayMood";
+            this.lblTodayMood.Size = new System.Drawing.Size(229, 16);
+            this.lblTodayMood.TabIndex = 0;
+            this.lblTodayMood.Text = "Bugünkü Ruh Haliniz: Henüz Girilmedi";
+            // 
+            // lblSuggestion
+            // 
+            this.lblSuggestion.AutoSize = true;
+            this.lblSuggestion.Location = new System.Drawing.Point(75, 109);
+            this.lblSuggestion.Name = "lblSuggestion";
+            this.lblSuggestion.Size = new System.Drawing.Size(313, 16);
+            this.lblSuggestion.TabIndex = 1;
+            this.lblSuggestion.Text = "Sistem Önerisi: Mod seçtiğinizde burada görünecek";
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(6, 6);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.RowHeadersWidth = 51;
+            this.dgvHistory.RowTemplate.Height = 24;
+            this.dgvHistory.Size = new System.Drawing.Size(726, 388);
+            this.dgvHistory.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MoodEntry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MoodEntry.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl MoodEntry;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ComboBox cmbMood;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSuggestion;
+        private System.Windows.Forms.Label lblTodayMood;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvHistory;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace MoodTrackerSystem
+{
+    public partial class Form1 : Form
+    {
+        // SQL Server kilitlenmelerini aşmak için RAM üzerinde çalışan sanal tablolar oluşturuyoruz
+        private DataTable dtUsers;
+        private DataTable dtMoodRecords;
+        private DataTable dtNotifications;
+
+        private int currentUserId = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeVirtualDatabase();
+        }
+
+        // Bilgisayardaki SQL servisinden bağımsız, RAM üzerinde çalışan sanal veritabanı kurulumu
+        private void InitializeVirtualDatabase()
+        {
+            // 1. USERS Tablosu Yapısı
+            dtUsers = new DataTable("USERS");
+            dtUsers.Columns.Add("user_id", typeof(int));
+            dtUsers.Columns.Add("ad", typeof(string));
+            dtUsers.Columns.Add("yaş", typeof(int));
+            dtUsers.Columns.Add("email", typeof(string));
+
+            // Test Kullanıcısı Ekleme
+            dtUsers.Rows.Add(currentUserId, "Test Kullanıcısı", 20, "test@email.com");
+
+            // 2. MOOD RECORDS Tablosu Yapısı
+            dtMoodRecords = new DataTable("MOOD_RECORDS");
+            dtMoodRecords.Columns.Add("record_id", typeof(int));
+            dtMoodRecords.Columns.Add("user_id", typeof(int));
+            dtMoodRecords.Columns.Add("tarih", typeof(DateTime));
+            dtMoodRecords.Columns.Add("ruh_hali", typeof(string));
+            dtMoodRecords.Columns.Add("not", typeof(string));
+
+            // 3. NOTIFICATIONS Tablosu Yapısı
+            dtNotifications = new DataTable("NOTIFICATIONS");
+            dtNotifications.Columns.Add("notification_id", typeof(int));
+            dtNotifications.Columns.Add("user_id", typeof(int));
+            dtNotifications.Columns.Add("tarih", typeof(DateTime));
+            dtNotifications.Columns.Add("mesaj", typeof(string));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadHistory();
+            UpdateDashboard();
+        }
+
+        // 1. KAYDET VE ÖNERİ AL BUTONU
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (cmbMood.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen önce bir ruh hali seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string selectedMood = cmbMood.SelectedItem.ToString();
+            string note = txtNote.Text;
+            DateTime now = DateTime.Now;
+
+         //   [cite_start]// Ödev Gereksinimi: Ruh haline göre dinamik destekleyici öneri belirleme algoritması [cite: 18, 22]
+            string suggestion = "";
+            switch (selectedMood)
+            {
+                case "Harika":
+                    suggestion = "Harika bir gün! Bu enerjiyi sevdiklerinle paylaşmaya ne dersin?";
+                    break;
+                case "Mutlu":
+                    suggestion = "Yüzündeki tebessümü koru. Bugün seni mutlu eden şeyleri bir kenara not et!";
+                    break;
+                case "Normal":
+                    suggestion = "Dengeli bir gün. Kısa bir yürüyüş ya da sevdiğin bir kahve günü daha da güzelleştirebilir.";
+                    break;
+                case "Stresli":
+                    suggestion = "Derin bir nefes al. 5 dakika gözlerini kapat ve sadece nefesine odaklan. Geçecek.";
+                    break;
+                case "Üzgün":
+                    suggestion = "Kendine yüklenme, her duygu insana dair. En sevdiğin müziği açıp biraz dinlenmeye ne dersin?";
+                    break;
+                default:
+                    suggestion = "Kendine zaman ayırmayı ve nazik davranmayı unutma.";
+                    break;
+            }
+
+            // Sanal Veritabanına Kayıt Ekleme (SQL injection ve kilitlenme riski sıfır)
+            int nextRecordId = dtMoodRecords.Rows.Count + 1;
+            dtMoodRecords.Rows.Add(nextRecordId, currentUserId, now, selectedMood, string.IsNullOrEmpty(note) ? "" : note);
+
+            int nextNotifyId = dtNotifications.Rows.Count + 1;
+            dtNotifications.Rows.Add(nextNotifyId, currentUserId, now, "Öneri: " + suggestion);
+
+            //[cite_start]// Arayüzü güncelle ve kullanıcıya göster [cite: 18, 22]
+            lblTodayMood.Text = "Bugünkü Ruh Haliniz: " + selectedMood;
+            lblSuggestion.Text = "Sistem Önerisi: " + suggestion;
+
+            MessageBox.Show("Ruh haliniz başarıyla kaydedildi ve analiz edildi!\n\nSistem Önerisi:\n" + suggestion, "Durum Analizi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Temizlik ve Ekranları Yenileme
+            txtNote.Clear();
+            cmbMood.SelectedIndex = -1;
+            LoadHistory();
+            UpdateDashboard();
+        }
+
+       // [cite_start]// 2. GEÇMİŞ KAYITLARI LİSTELEME FONKSİYONU [cite: 19]
+        private void LoadHistory()
+        {
+            // DataGridView için gösterim tablosu oluşturuyoruz
+            DataTable displayTable = new DataTable();
+            displayTable.Columns.Add("Kayıt Tarihi", typeof(DateTime));
+            displayTable.Columns.Add("Ruh Hali", typeof(string));
+            displayTable.Columns.Add("Kullanıcı Notu", typeof(string));
+
+            // Kayıtları tersten (en yeni üste gelecek şekilde) listeliyoruz
+            for (int i = dtMoodRecords.Rows.Count - 1; i >= 0; i--)
+            {
+                DataRow row = dtMoodRecords.Rows[i];
+                displayTable.Rows.Add(row["tarih"], row["ruh_hali"], row["not"]);
+            }
+
+            dgvHistory.DataSource = displayTable;
+        }
+
+       // [cite_start]// 3. ANA PANELİ GÜNCELLEME FONKSİYONU [cite: 33]
+        private void UpdateDashboard()
+        {
+            if (dtMoodRecords.Rows.Count > 0)
+            {
+                DataRow lastMood = dtMoodRecords.Rows[dtMoodRecords.Rows.Count - 1];
+                lblTodayMood.Text = "Son Girilen Ruh Haliniz: " + lastMood["ruh_hali"].ToString();
+            }
+
+            if (dtNotifications.Rows.Count > 0)
+            {
+                DataRow lastNotify = dtNotifications.Rows[dtNotifications.Rows.Count - 1];
+                string msg = lastNotify["mesaj"].ToString().Replace("Öneri: ", "");
+                lblSuggestion.Text = "Sistem Önerisi: " + msg;
+            }
+        }
+    }
+}
